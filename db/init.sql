d0a0fb8ccbd0905740de95fc2904e42ea590f387
togami2864 <tuabobo123@gmail.com>
2021-10-02 23:51:10 +0900

    feat: add docker environment

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION if not exists pgcrypto;
+CREATE TABLE IF NOT EXISTS poche_items (
+    id uuid DEFAULT gen_random_uuid(),
+    title VARCHAR(100) NOT NULL,
+    url VARCHAR(2083) NOT NULL,
+    memo VARCHAR(1024),
+    date DATE DEFAULT now() NOT NULL,
+    tags VARCHAR(100)[],
+    private boolean NOT NULL
+)
